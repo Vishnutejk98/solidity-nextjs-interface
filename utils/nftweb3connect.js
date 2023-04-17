93cfc3fa5839727635727e718fe18421db0c3f74
vishnutejk98 <mailboxnumber04@gmail.com>
2023-04-17 10:11:29 +0530

    Hard codeded the URLs

--- utils/nftweb3connect.js
+++ utils/nftweb3connect.js
@@ -2,7 +2,11 @@ import Web3 from "web3";
 
 const NFT_NODE_URL = process.env.NFT_NODE_URL;
 console.log(NFT_NODE_URL);
-const web3 = new Web3(new Web3.providers.HttpProvider(NFT_NODE_URL));
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://polygon-mumbai.g.alchemy.com/v2/w0zL4yHDrVP5FMYVJvrjOtxBVGQMm72o"
+  )
+);
 
 module.exports = {
   web3,
--- utils/web3connect.js
+++ utils/web3connect.js
@@ -1,7 +1,11 @@
 import Web3 from "web3";
 
 const NODE_URL = process.env.NODE_URL;
-const web3 = new Web3(new Web3.providers.HttpProvider(NODE_URL));
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://sepolia.infura.io/v3/4aaeeb718e564127b5828bb4e92b5936"
+  )
+);
 
 module.exports = {
   web3,
